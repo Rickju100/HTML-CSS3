01ab2119ffaa5ae7b631c90bdbf30f57d3583112
Rickju100 <ricardo.funez95@gmail.com>
2024-08-21 00:07:03 -0600

    Third

--- Guess the number/JS/scripts.js
+++ Guess the number/JS/scripts.js
@@ -1,32 +1,85 @@
-let randomNumber = Math.floor(Math.random(100*100))
+let randomNumber = 0
 
-function contador(){
-    let counter = document.getElementById("#count")
-    counter ++;
-    counter.innerHTML="counter"
+function randomizer() {
+    randomNumber = Math.floor(Math.random(100) * 100)
+    console.log(randomNumber)
+}
+
+let counter = 0
+function contador() {
+    counter++;
+    document.getElementById("count").innerHTML = counter;
+
 }
 
 
-function iniciando(){
-    let div1 = document.querySelector("#div1")
-    let div2 = document.querySelector("#div2")
-    if(div1.style.display === "flex"){
-        div1.style.display = "none"
-        if(div1.style.display === "none"){
-            div2.style.display = "flex"
-        }
+function evaluar() {
+    let input_value = document.getElementById("input").value.trim(); 
+
+ 
+    let feedbackElement = document.getElementById("feedback");
+
+ 
+    if (input_value === "") {
+        feedbackElement.innerHTML = "Please enter a value.";
+        feedbackElement.style.color = "red";
+        return; 
     }
 
+    
+    if (isNaN(input_value)) {
+        feedbackElement.innerHTML = "Please enter a valid number.";
+        feedbackElement.style.color = "red";
+        return; 
+    }
+
+    input_value = parseInt(input_value); 
+
+    contador(); 
+
+
+    if (input_value === randomNumber) {
+        console.log("prueba");
+        let div1 = document.getElementById("div1");
+        let div2 = document.getElementById("div2");
+        let div3 = document.getElementById("div3");
+
+        div2.style.display = 'none';
+        div1.style.display = 'flex';
+        div3.style.display = 'flex';
+
+        document.getElementById("attemps").innerHTML = counter;
+        document.getElementById("random").innerHTML = randomNumber;
+    }
+
+   
+    if (input_value < randomNumber) {
+        feedbackElement.innerHTML = "Value " + input_value + " is too low, try again.";
+        feedbackElement.style.color = "yellow";
+    }
+
+   
+    if (input_value > randomNumber) {
+        feedbackElement.innerHTML = "Value " + input_value + " is too high, try again.";
+        feedbackElement.style.color = "yellow";
+    }
 }
 
+function iniciando() {
+    let div1 = document.getElementById("div1")
+    let div2 = document.getElementById("div2")
+    let div3 = document.getElementById("div3")
+    randomizer()
+    if (div1.style.display == 'flex') {
+        div1.style.display = 'none'
+        div2.style.display = 'flex'
+    }
 
-let p_user_number = document.getElementById("txt_number");    
+    if (div3.style.display == 'flex') {
+        div3.style.display = 'none'
+    }
 
-let user_number = p_user_number.value;
-
-if (user_number.length === 0) 
-{
-
-
+    counter = 0
+    document.getElementById("count").innerHTML = counter;
 }
 
