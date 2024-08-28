37b157ccc55d28e5cfa27e44105adbc4ebb29109
Rickju100 <ricardo.funez95@gmail.com>
2024-08-27 19:13:52 -0600

    Review4

--- Musical Keyboard/JS/scripts.js
+++ Musical Keyboard/JS/scripts.js
@@ -2,6 +2,7 @@ window.addEventListener("keydown", handleKeydown);
 window.addEventListener("click", handleOnClick);
 
 function handleKeydown(eventDetails) {
+  sizing();
   const key = eventDetails.key.toLowerCase(); 
   const audiofile = document.getElementById(eventDetails.key);
   audiofile.currentTime = 0;
@@ -9,20 +10,24 @@ function handleKeydown(eventDetails) {
 
   
 
-  let button = document.getElementById(`b${key}`);
-  if (button) {
-    button.style.animationIterationCount = "1";
-    button.style.animationName = "sizer"; 
-    button.style.animationDuration = "1s";
-
-   
-    button.addEventListener('animationend', () => {
-      button.style.animationName = "";
-    });
-  }
 } 
 
+function sizing() {
+  const letras = ["a", "s", "d", "f", "g", "h", "j", "k", "l"];
+  letras.forEach((key) => {
+    let button = document.getElementById(`b${key}`);
+    if (button) {
+      button.style.animationIterationCount = "1";
+      button.style.animationName = "sizer"; 
+      button.style.animationDuration = "1s";
 
+      button.addEventListener('animationend', () => {
+        button.style.animationName = "";
+      });
+    }
+  });
+}
+  
 
 function handleOnClick(eventDetails) {
   let element = document.getElementById(eventDetails.target.id).value;
