7cc2b07ad3bd7bd0526dc8d7a7f51862633aa90c
Rickju100 <ricardo.funez95@gmail.com>
2024-09-09 20:52:30 -0600

    FInal Project Update

--- FINAL PPROJECT/SCRIPTS/script.js
+++ FINAL PPROJECT/SCRIPTS/script.js
@@ -1,23 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() { /* in the research i did, this will help the code to lade after the dom has loaded*/
+    const apiURL = "https://fakestoreapi.com/products";
+    console.log(apiURL)
 
-const apiURL = "https://fakestoreapi.com/products";
-
-
-
-function product(data){
-    let container = document.getElementById("Productos");
-
-    for (const producto of data.result){
-        let item = document.createElement("div");
-        item.classList.add("item")
-        item.innerHTML = producto.name.toUpperCase();
-        container.appendChild(item);
-
+    function product(data){
+        let container = document.getElementById("Productos");
+        console.log(container)
+        for (const producto of data.result){
+            let item = document.createElement("div");
+            item.classList.add("item");
+            item.innerHTML = producto.name.toUpperCase();
+            container.appendChild(item);
+        }
     }
-}
 
-fetch(apiURL)
-.then((promise) => promise.json())
-.then(function (result) {
-    handleResult(result);
+    fetch(apiURL)
+        .then((promise) => promise.json())
+        .then(function (result) {
+            product(result);
+        })
+    
 });
-
